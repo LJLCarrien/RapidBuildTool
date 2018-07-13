00af82abaf3af132fab67f6bc0da98d14c9d471a
LJLCarrien <377262335@qq.com>
2018-07-13 18:14:36 +0800

    Add Lbl添加修改名字、位置、筛选功能

--- Assets/Editor/RapidBuildEditorTool.cs
+++ Assets/Editor/RapidBuildEditorTool.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEditor;
 
 public class RapidBuildEditorTool : EditorWindow
@@ -14,45 +15,56 @@ public class RapidBuildEditorTool : EditorWindow
     }
 
     private GameObject mTarget;
+    private bool lblFoldout;
     void OnGUI()
     {
-        EditorGUILayout.HelpBox("创建UI:", MessageType.Info);
+        EditorGUILayout.HelpBox("创建对象:", MessageType.Info, true);
         DrawVertical(() =>
         {
             DrawButton("创建按钮", CreateButton, 100);
-            isNeedScale = GUILayout.Toggle(isNeedScale, "需要缩放");
+            isNeedScale = GUILayout.Toggle(isNeedScale, "点击需要缩放效果");
         }, "Box");
 
-        EditorGUILayout.HelpBox("修改UI:", MessageType.Info);
+        EditorGUILayout.HelpBox("针对此对象的子物体修改:", MessageType.Info);
         DrawVertical(() =>
         {
-            mTarget = Selection.activeGameObject;
-            mTarget = EditorGUILayout.ObjectField("啥UI：", mTarget, typeof(GameObject), true, GUILayout.ExpandWidth(true)) as GameObject;
+            if (mTarget == null) mTarget = Selection.activeGameObject;
+            mTarget = EditorGUILayout.ObjectField("修改对象：", mTarget, typeof(GameObject), true, GUILayout.ExpandWidth(true)) as GameObject;
             if (mTarget != null)
             {
+                DrawHorizontal(() =>
+                {
+                    isNeedChangeName = GUILayout.Toggle(isNeedChangeName, "修改名字");
+                    isNeedChangeTs = GUILayout.Toggle(isNeedChangeTs, "修改位置");
+                });
+                isOnlyShowDepthBelowTen = GUILayout.Toggle(isOnlyShowDepthBelowTen, "只显示层级低于10");
+
+                DrawButton("一键修正缩放", OneKeyResetScale, 100);
+                if (resultStringBuilder != null && resultStringBuilder.Length > 0)
+                    GUILayout.TextArea(resultStringBuilder.ToString());
+
                 DrawHorizontal(() =>
                 {
                     DrawButton("获取Label", GetLabels, 100);
                     DrawButton("获取所有Sprite", GetSprites, 100);
                     DrawButton("获取所有Texture", GetTextures, 100);
                 });
-                DrawHorizontal(() =>
+
+                //labels
+                if (childLblList != null && childLblList.Count != 0)
                 {
-                    foreach (var itemLbl in childLblList)
+                    lblFoldout = EditorGUILayout.Foldout(lblFoldout, "Labels");
+                    if (lblFoldout)
                     {
-                        if (componnent2ToggoleDic.ContainsKey(itemLbl))
+                        DrawVertical(() =>
                         {
-                            DrawToggleGroup(itemLbl, componnent2ToggoleDic[itemLbl]);
-                        }
-                        else
-                        {
-                            
-                            DrawToggleGroup(itemLbl, itemLbl.gameObject.activeSelf);
-
-                        }
+                            foreach (var itemLbl in childLblList)
+                            {
+                                DrawToggleItem(itemLbl, itemLbl.gameObject.activeSelf);
+                            }
+                        }, "Box");
                     }
-                });
-
+                }
             }
         }, "Box");
     }
@@ -86,7 +98,7 @@ public class RapidBuildEditorTool : EditorWindow
             var btnLbl = NGUISettings.AddLabel(goParent);
             btnLbl.name = "BtnLbl";
             btnLbl.applyGradient = false;
-            btnLbl.depth = 10;
+            btnLbl.depth = LabelDefaultDepth;
 
             btnLbl.transform.SetParent(btn.transform);
         }
@@ -95,7 +107,7 @@ public class RapidBuildEditorTool : EditorWindow
     /// <summary>
     /// 是否需要按钮点击缩放
     /// </summary>
-    private bool isNeedScale;
+    private bool isNeedScale = true;
     /// <summary>
     /// 按钮所需的其他设置
     /// </summary>
@@ -110,7 +122,20 @@ public class RapidBuildEditorTool : EditorWindow
         }
 
     }
+    /// <summary>
+    /// 是否需要提供修改名字
+    /// </summary>
+    private bool isNeedChangeName = true;
+    /// <summary>
+    /// 是否需要提供修改位置
+    /// </summary>
+    private bool isNeedChangeTs = true;
+    /// <summary>
+    /// 是否只显示层级低于10的
+    /// </summary>
+    private bool isOnlyShowDepthBelowTen;
 
+    private readonly int LabelDefaultDepth = 10;
     private List<UILabel> childLblList;
     /// <summary>
     /// 获得指定物品下的所有带Label组件的子物体
@@ -120,7 +145,25 @@ public class RapidBuildEditorTool : EditorWindow
         childLblList = new List<UILabel>();
         var tmpList = GetChildsWithThis<UILabel>();
         if (tmpList != null)
-            childLblList.AddRange(tmpList);
+        {
+            if (isOnlyShowDepthBelowTen)
+            {
+                var tmpDepthBelowTenList = new List<UILabel>();
+                foreach (var uiLabel in tmpList)
+                {
+                    if (uiLabel.depth < LabelDefaultDepth)
+                    {
+                        tmpDepthBelowTenList.Add(uiLabel);
+                    }
+
+                }
+                childLblList.AddRange(tmpDepthBelowTenList);
+
+            }
+            else
+                childLblList.AddRange(tmpList);
+        }
+
         Debug.LogError(childLblList.Count);
     }
 
@@ -158,12 +201,35 @@ public class RapidBuildEditorTool : EditorWindow
         if (chilTList.Count > 0) return chilTList;
         else return null;
     }
+
+    private StringBuilder resultStringBuilder;
+    private readonly Vector3 needResetToThisScale = Vector3.one;
+    /// <summary>
+    /// 还原所有子物体的缩放为1
+    /// </summary>
+    private void OneKeyResetScale()
+    {
+        if (mTarget != null)
+        {
+            resultStringBuilder = new StringBuilder();
+            for (int i = 0; i < mTarget.transform.childCount; i++)
+            {
+                var childTs = mTarget.transform.GetChild(i);
+                if (childTs.localScale != needResetToThisScale)
+                {
+                    var oldLocalScale = childTs.localScale;
+                    childTs.localScale = needResetToThisScale;
+                    resultStringBuilder.AppendLine(childTs.name + " 旧:" + oldLocalScale + " 新:" + childTs.localScale);
+                }
+            }
+        }
+    }
     #endregion
 
     #region 辅助
-    void DrawButton(string cName, Action cClickAction, int cWidth)
+    void DrawButton(string cName, Action cClickAction, int cWidth, int cHeight = 50)
     {
-        if (GUILayout.Button(cName, GUILayout.Width(cWidth), GUILayout.Height(50)))
+        if (GUILayout.Button(cName, GUILayout.Width(cWidth), GUILayout.Height(cHeight)))
         {
             if (cClickAction != null) cClickAction();
         }
@@ -189,12 +255,50 @@ public class RapidBuildEditorTool : EditorWindow
         }
     }
 
-    Dictionary<UIWidget, bool> componnent2ToggoleDic = new Dictionary<UIWidget, bool>();
-    void DrawToggleGroup(UIWidget widget, bool toggle)
+    //原始名字字典
+    Dictionary<UIWidget, string> originalNameDic = new Dictionary<UIWidget, string>();
+    //原始位置字典
+    Dictionary<UIWidget, Vector2> originalTsDic = new Dictionary<UIWidget, Vector2>();
+    /// <summary>
+    /// 绘单个Item
+    /// </summary>
+    /// <param name="widget"></param>
+    /// <param name="toggle"></param>
+    void DrawToggleItem(UIWidget widget, bool toggle)
     {
         var bIsToggle = EditorGUILayout.BeginToggleGroup(widget.name, toggle);
-        componnent2ToggoleDic.Add(widget, bIsToggle);
-        //widget.gameObject.SetActive(toggle);
+        widget.gameObject.SetActive(bIsToggle);
+        DrawHorizontal(() =>
+        {
+            if (isNeedChangeName)
+            {
+                var widgetName = EditorGUILayout.TextField("name:", widget.name);
+                widget.name = widgetName;
+                if (!originalNameDic.ContainsKey(widget)) originalNameDic.Add(widget, widget.name);
+                DrawButton("还原", () =>
+                {
+                    if (originalNameDic.ContainsKey(widget))
+                        widget.name = originalNameDic[widget];
+                }, 50, 20);
+            }
+        });
+        DrawHorizontal(() =>
+        {
+            if (isNeedChangeTs)
+            {
+                var widgetTs = EditorGUILayout.Vector2Field("Transform:", new Vector2(widget.transform.localPosition.x, widget.transform.localPosition.y));
+                widget.transform.localPosition = widgetTs;
+                if (!originalTsDic.ContainsKey(widget)) originalTsDic.Add(widget, widgetTs);
+                DrawButton("还原", () =>
+                {
+                    if (originalTsDic.ContainsKey(widget))
+                    {
+                        widget.transform.localPosition = originalTsDic[widget];
+                    }
+                }, 50, 20);
+            }
+        });
+        EditorGUILayout.EndToggleGroup();
     }
     #endregion
 
